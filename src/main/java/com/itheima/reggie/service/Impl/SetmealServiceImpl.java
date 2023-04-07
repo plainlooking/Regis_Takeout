849a9c61d6a9a65dc3f6ae1c348037a04147c1b7
luobo <3159868825@qq.com>
2023-04-07 22:24:33 +0800

    Initial commit

---
+++ src/main/java/com/itheima/reggie/service/Impl/SetmealServiceImpl.java
@@ -0,0 +1,50 @@
+package com.itheima.reggie.service.Impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.dto.SetmealDto;
+import com.itheima.reggie.mapper.SetmealMapper;
+import com.itheima.reggie.pojo.Setmeal;
+import com.itheima.reggie.pojo.SetmealDish;
+import com.itheima.reggie.service.SetmealDishService;
+import com.itheima.reggie.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> implements SetmealService {
+    @Autowired
+    private SetmealDishService setmealDishService;
+
+    /**
+     * 新增套餐，同时需要保存套餐和菜品的关联关系；
+     * @param setmealDto
+     */
+    @Transactional //加入事务注解，保证数据一致性
+    @Override
+    public void saveWithDish(SetmealDto setmealDto) {
+        //保存套餐的基本信息，操作setmeal，执行insert
+        this.save(setmealDto);
+
+        List<SetmealDish> setmealDishes = setmealDto.getSetmealDishes();
+        setmealDishes.stream().map((item)->{
+            item.setSetmealId(setmealDto.getId());
+            return item;
+        }).collect(Collectors.toList());
+
+        //保存套餐和菜品的关联信息，操作setmeal_dish,执行insert操作
+        setmealDishService.saveBatch(setmealDishes);
+
+    }
+
+    @Override
+    public void removeWithDish(List<Long> ids) {
+
+    }
+}
